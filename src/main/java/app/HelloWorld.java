10a00e485c162442397cdfa85fbbca8a9ae641e5
woodser <woodser@protonmail.com>
2019-12-18 10:00:37 -0500

    commit HelloWorld.java which is sample app

---
+++ src/main/java/app/HelloWorld.java
@@ -0,0 +1,11 @@
+package app;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    
+    MoneroWalletRpc walletRpc = new MoneroWalletRpc("http://localhost:38083", "rpc_user", "abc123");
+    System.out.println("Height: " + walletRpc.getHeight());
+  }
+}
