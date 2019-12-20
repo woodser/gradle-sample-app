941dc98287846c4c446bbbf33bdddfa26c6a376c
woodser <woodser@protonmail.com>
2019-12-20 09:01:51 -0500

    project building as maven project

--- src/main/java/app/HelloWorld.java
+++ src/main/java/app/HelloWorld.java
@@ -1,17 +1,12 @@
 package app;
 
-import com.google.gson.Gson;
 import monero.wallet.MoneroWalletRpc;
 
 public class HelloWorld {
 
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    
-    System.out.println("Gson?");
-    Gson gson = new Gson();
     MoneroWalletRpc walletRpc = new MoneroWalletRpc("http://localhost:38083", "rpc_user", "abc123");
     System.out.println("Height: " + walletRpc.getHeight());
-    System.out.println("Gson!");
   }
 }
