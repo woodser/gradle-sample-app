b7238793b2b4517af0f49dd727e011ef40e66f64
woodser <woodser@protonmail.com>
2020-06-05 08:08:31 -0400

    update to monero-java-v0.3.0

--- src/main/java/app/HelloWorld.java
+++ src/main/java/app/HelloWorld.java
@@ -1,6 +1,9 @@
 package app;
 
+import monero.daemon.model.MoneroNetworkType;
+import monero.wallet.MoneroWalletJni;
 import monero.wallet.MoneroWalletRpc;
+import monero.wallet.model.MoneroWalletConfig;
 
 public class HelloWorld {
 
@@ -8,5 +11,8 @@ public class HelloWorld {
     System.out.println("Hello world!");
     MoneroWalletRpc walletRpc = new MoneroWalletRpc("http://localhost:38083", "rpc_user", "abc123");
     System.out.println("Height: " + walletRpc.getHeight());
+    
+    MoneroWalletJni walletJni = MoneroWalletJni.createWallet(new MoneroWalletConfig().setPath("./test_wallets/my_test_wallet").setPassword("abctesting123").setNetworkType(MoneroNetworkType.STAGENET));
+    System.out.println(walletJni.getPrimaryAddress());
   }
 }
